1a4fcfd3f139475435c40f28ccd40c6f8d30a75a
Hugo Cruz <hugocruzlfc@gmail.com>
2026-05-25 13:15:40 +0200

    feat: setup tailwindcss and add header components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '@/components/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

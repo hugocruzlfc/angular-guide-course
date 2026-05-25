0e35fd62799b3252370cadd8e67c9efd07ed1059
Hugo Cruz <hugocruzlfc@gmail.com>
2026-05-25 14:06:15 +0200

    feat: add user array interpolation

--- src/app/features/user/user.ts
+++ src/app/features/user/user.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { USERS } from '@/app/shared/constants';
+
+const randomUserIndex = Math.floor(Math.random() * USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.html',
 })
-export class UserComponent {}
+export class UserComponent {
+  selectedUser = USERS[randomUserIndex];
+}

92069b0646222271b0f5edfcd896481fd661536b
Hugo Cruz de la Torre <hugocruzlfc@gmail.com>
2026-06-11 15:17:05 +0200

    feat: add control shared component

--- advance-project/src/app/features/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ advance-project/src/app/features/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,3 +1,4 @@
+import { ControlComponent } from '@/app/shared/control/control.component';
 import { ButtonComponent } from '@/app/shared/ui/button/button.component';
 import { Component } from '@angular/core';
 import { LucideSendHorizontal } from '@lucide/angular';
@@ -5,7 +6,7 @@ import { LucideSendHorizontal } from '@lucide/angular';
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, LucideSendHorizontal],
+  imports: [ButtonComponent, LucideSendHorizontal, ControlComponent],
   templateUrl: './new-ticket.component.html',
 })
 export class NewTicketComponent {}

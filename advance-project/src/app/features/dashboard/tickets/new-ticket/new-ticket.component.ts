c06ed37fa61958c8b590b41335521f6b3037cbbe
Hugo Cruz <hugocruzlfc@gmail.com>
2026-06-15 17:12:52 +0200

    feat: add view template

--- advance-project/src/app/features/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ advance-project/src/app/features/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,33 @@
 import { ControlComponent } from '@/app/shared/control/control.component';
 import { ButtonComponent } from '@/app/shared/ui/button/button.component';
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { LucideSendHorizontal } from '@lucide/angular';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, LucideSendHorizontal, ControlComponent],
+  imports: [
+    ButtonComponent,
+    LucideSendHorizontal,
+    ControlComponent,
+    FormsModule,
+  ],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;  old version
+
+  private form = viewChild<ElementRef<HTMLFormElement>>('form');
+
+  // form: HTMLFormElement could be pass via template reference
+  onSubmit(titleInput: string, requestInput: string) {
+    console.log(titleInput);
+    console.log(requestInput);
+
+    // this.form?.nativeElement.reset();
+
+    this.form()?.nativeElement.reset(); // new version
+  }
+}

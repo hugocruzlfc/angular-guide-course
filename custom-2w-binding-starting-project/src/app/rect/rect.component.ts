00709cdbdb6598f8473a45f9008b09700e554db5
Hugo Cruz <hugocruzlfc@gmail.com>
2026-07-13 16:39:12 +0200

    feat: add 2 way binding

---
+++ custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, model, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-rect',
+  standalone: true,
+  imports: [],
+  templateUrl: './rect.component.html',
+  styleUrl: './rect.component.css',
+})
+export class RectComponent {
+  // old version
+  // @Input({ required: true }) size!: { width: string; height: string };
+  // @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
+
+  size = model.required<{ width: string; height: string }>();
+
+  onReset() {
+    // old version
+    //this.sizeChange.emit({ width: '200', height: '100' });
+
+    this.size.set({ width: '200', height: '100' });
+  }
+}

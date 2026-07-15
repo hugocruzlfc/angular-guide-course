6d2fc8390c7e2e3b104787415bd49cab1ab7d5b2
Hugo Cruz <hugocruzlfc@gmail.com>
2026-07-15 17:51:16 +0200

    feat: add directive with inject dependencie

--- directives-deep-dive/src/app/directives/safe-link.directive.ts
+++ directives-deep-dive/src/app/directives/safe-link.directive.ts
@@ -1,11 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, inject, input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]', // combined with the <a> tag
   standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
 export class SafeLinkDirective {
+  queryParams = input('my-app', { alias: 'appSafeLink' });
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
   constructor() {
     console.log('SafeLinkDirective created!');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = confirm('Are you sure you want to leave this page?');
+
+    if (!wantsToLeave) {
+      event.preventDefault(); // Prevent the default action of the
+    }
+
+    //const address = (event.target as HTMLAnchorElement).href;
+    const address = this.hostElementRef.nativeElement.href;
+    // (event.target as HTMLAnchorElement).href =
+    //   `${address}?from=${this.queryParams()}`;
+    this.hostElementRef.nativeElement.href = `${address}?from=${this.queryParams()}`;
   }
 }

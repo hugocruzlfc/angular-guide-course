c16dd218300d702fdc6428cdb1d7cb4a57d9a0b9
Hugo Cruz <hugocruzlfc@gmail.com>
2026-07-31 11:46:09 +0200

    feat: remove zone.js provider

--- change-detection/src/app/counter/counter.component.ts
+++ change-detection/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, NgZone, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -8,10 +8,11 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterComponent {
   //NgZone is a service offer a method we allow exclude of zone.js for specific code.
-  private zone = inject(NgZone);
+  //private zone = inject(NgZone);
   count = signal(0);
 
   get debugOutput() {
@@ -24,11 +25,15 @@ export class CounterComponent {
       this.count.set(0);
     }, 4000);
 
-    this.zone.runOutsideAngular(() => {
-      setTimeout(() => {
-        console.log('Timer expired');
-      }, 4000);
-    });
+    // this.zone.runOutsideAngular(() => {
+    //   setTimeout(() => {
+    //     console.log('Timer expired');
+    //   }, 4000);
+    // });
+
+    setTimeout(() => {
+      console.log('Time expired');
+    }, 5000);
   }
 
   onDecrement() {

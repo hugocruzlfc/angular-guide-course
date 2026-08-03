97d03601faefd8affb51333a95a66e2a7d988440
Hugo Cruz <hugocruzlfc@gmail.com>
2026-08-03 12:48:55 +0200

    feat: add places full service

--- http-request-responses/src/app/places/places.service.ts
+++ http-request-responses/src/app/places/places.service.ts
@@ -1,23 +1,49 @@
 import { inject, Injectable, signal } from '@angular/core';
 
-import { Place } from './place.model';
+import { GetPlacesResponse, Place } from './place.model';
 import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
+
+const API_BASE_URL = 'http://localhost:3000';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
-  private userPlaces = signal<Place[]>([]);
-
   private htpClient = inject(HttpClient);
+  private userPlaces = signal<Place[]>([]);
 
   loadedUserPlaces = this.userPlaces.asReadonly();
 
-  loadAvailablePlaces() {}
+  loadAvailablePlaces() {
+    return this.fetchPlaces(
+      `${API_BASE_URL}/places`,
+      'Something went wrong fetching the available places. Please try again',
+    );
+  }
 
-  loadUserPlaces() {}
+  loadUserPlaces() {
+    return this.fetchPlaces(
+      `${API_BASE_URL}/user-places`,
+      'Something went wrong fetching your favorite places. Please try again',
+    );
+  }
 
-  addPlaceToUserPlaces(place: Place) {}
+  addPlaceToUserPlaces(placeId: string) {
+    return this.htpClient.put(`${API_BASE_URL}/user-places`, {
+      placeId,
+    });
+  }
 
   removeUserPlace(place: Place) {}
+
+  private fetchPlaces(url: string, errorMessage: string) {
+    return this.htpClient.get<GetPlacesResponse>(url).pipe(
+      map((resData) => resData.places),
+      catchError((error) => {
+        console.log(error);
+        return throwError(() => new Error(errorMessage));
+      }),
+    );
+  }
 }

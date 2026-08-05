7a4ce7a43f57d52812d3807b182a7536277a068e
Hugo Cruz <hugocruzlfc@gmail.com>
2026-08-05 13:55:05 +0200

    feat: add custom validator

--- forms-template-driven-reactive/src/app/auth/react-form/login/login.component.ts
+++ forms-template-driven-reactive/src/app/auth/react-form/login/login.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+
+function mustContainQuestionMark(control: AbstractControl) {
+  if (control.value.includes('?')) {
+    return null;
+  }
+
+  return {
+    doesNotContainQuestionMark: true,
+  };
+}
 
 @Component({
   selector: 'app-react-login',
@@ -19,7 +30,11 @@ export class ReactLoginComponent {
       validators: [Validators.email, Validators.required],
     }),
     password: new FormControl('', {
-      validators: [Validators.required, Validators.min(6)],
+      validators: [
+        Validators.required,
+        Validators.min(6),
+        mustContainQuestionMark,
+      ],
     }),
   });
 

7a50a6b7e3a26c899fcdf8212cef8fc8e8573edb
Hugo Cruz <hugocruzlfc@gmail.com>
2026-08-07 15:03:28 +0200

    feat: add routing config

---
+++ routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -0,0 +1,17 @@
+import { Component, computed, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
+
+@Component({
+  selector: 'app-user-tasks',
+  standalone: true,
+  templateUrl: './user-tasks.component.html',
+  styleUrl: './user-tasks.component.css',
+})
+export class UserTasksComponent {
+  userId = input.required<'string'>();
+  private userService = inject(UsersService);
+
+  userName = computed(
+    () => this.userService.users.find((u) => u.id === this.userId())?.name,
+  );
+}

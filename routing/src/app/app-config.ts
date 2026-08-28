0501a96669c9722f40c9d85472f9019f238caf30
Hugo Cruz <hugocruzlfc@gmail.com>
2026-08-28 12:20:17 +0200

    feat: add children routing

--- routing/src/app/app-config.ts
+++ routing/src/app/app-config.ts
@@ -1,7 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withRouterConfig,
+} from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding())],
+  providers: [
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withRouterConfig({ paramsInheritanceStrategy: 'always' }),
+      //necessary for the new router to work with the input decorator in child routes
+    ),
+  ],
 };
--- routing/src/app/users/user-tasks/user-tasks.component.ts
+++ routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,17 +1,46 @@
-import { Component, computed, inject, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  inject,
+  input,
+  OnInit,
+} from '@angular/core';
 import { UsersService } from '../users.service';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet],
 })
-export class UserTasksComponent {
+export class UserTasksComponent implements OnInit {
   userId = input.required<'string'>();
   private userService = inject(UsersService);
+
+  ///old version, necessary activate OnInit
+  private activatedRoute = inject(ActivatedRoute);
+  userNameV1 = '';
+  private destroyRef = inject(DestroyRef);
+  ////
 
   userName = computed(
     () => this.userService.users.find((u) => u.id === this.userId())?.name,
   );
+
+  ngOnInit() {
+    const subscription = this.activatedRoute.params.subscribe({
+      next: (paramMap) => {
+        this.userNameV1 =
+          this.userService.users.find((u) => u.id === paramMap['userId'])
+            ?.name || '';
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
